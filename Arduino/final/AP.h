9b2a0bfd4ed2f403f4ba7728b3e89b57c317ee31
alyashour <79553148+alyashour@users.noreply.github.com>
2023-03-29 18:19:39 -0400

    Create AP.h

---
+++ Arduino/final/AP.h
@@ -0,0 +1,31 @@
+#ifndef AP_h
+#define AP_h
+
+#include <WiFiNINA.h>
+
+extern char ssid[]; // your network SSID (name)
+extern char pass[]; // your network password (use for WPA, or use as key for WEP)
+extern int status;
+extern WiFiServer server;
+extern String clientMessage;
+extern bool flash;
+
+extern String input_ssid;
+extern String input_username;
+extern String input_pass;
+extern bool enterpriseNetwork;
+
+void setupSVR();
+
+void setup_AP();
+bool loop_AP(bool &doAP);
+void printWiFiStatus();
+
+/* My baby */
+void html_AP(WiFiClient client);
+String getSubstring(String inputString, String startWord, String endWord);
+void sendResponse(WiFiClient client);
+
+bool isEnterprise();
+
+#endif
